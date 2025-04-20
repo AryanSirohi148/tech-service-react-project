6545b1cc124dab66c919fc5e516b5efdfed59af8
Aryan Sirohi <aryansirohi@Aryans-MacBook-Air.local>
2025-04-20 19:08:36 +0530

    Enhanced HeroSection with additional styling and image

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
+import HeroSection from '../components/HeroSection'
 
 const Home = () => {
   return (
-    <div>
-        home
-      
-    </div>
+    <HeroSection/>
   )
 }
 
